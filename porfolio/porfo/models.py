2bd687b2e9da4fdede0f0b160e78316d59e939b3
ManuRodriguezC <estebanclimb@gmail.com>
2023-03-23 15:26:49 -0500

    Created teh arquitecture with divs

--- porfolio/porfo/models.py
+++ porfolio/porfo/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=40)
+    description = models.TextField()
+    image = models.ImageField(default="null", upload_to="images")
+    tools = models.CharField(max_length=100)
+    github = models.URLField(blank=True)
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+class Skill(models.Model):
+    title = models.CharField(max_length=40)
+    image = models.ImageField(default="null", upload_to="images")
+
+    def __str__(self):
+        return f"{self.title}"
+
--- porfolio/porfolio/settings.py
+++ porfolio/porfolio/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'porfo'
 ]
 
 MIDDLEWARE = [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
 TIME_ZONE = 'UTC'
 
@@ -116,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOR = BASE_DIR / 'media'
+
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
