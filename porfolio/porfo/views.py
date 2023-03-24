9c807401b2cbeca5e3ecf361409027ccf45cd288
ManuRodriguezC <estebanclimb@gmail.com>
2023-03-23 21:45:27 -0500

    Create models for projects, skills and dev front and design

--- porfolio/porfo/views.py
+++ porfolio/porfo/views.py
@@ -3,5 +3,12 @@ from .models import Project, Skill
 
 def home(request):
     projects = Project.objects.all()
+    skills = Skill.objects.all()
     return render(request, 'home.html',
-                  {'projects': projects})
+                  {'projects': projects,
+                   'skills': skills}
+    )
+
+def projects(request, project_id):
+    project = Project.objects.get(id=project_id)
+    return render(request, 'project.html', {'project': project})
--- porfolio/porfolio/urls.py
+++ porfolio/porfolio/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('<int:project_id>', views.projects, name="project")
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
